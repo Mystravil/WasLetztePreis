5a91b614df7fb64bdde657d80cadedf1f6daf838
DGEN <dorian.genich@pure-lox.com>
2021-01-06 12:06:23 +0100

    Removes type checking in advert.py

--- backend/app/app/models/advert.py
+++ backend/app/app/models/advert.py
@@ -8,9 +8,6 @@ from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
-if TYPE_CHECKING:
-    from .item import Item  # noqa: F401
-
 
 class Advert(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
